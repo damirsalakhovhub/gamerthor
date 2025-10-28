e7def76eb7d8cb2e58a69c26dd4288b4a2e45337
Damir Salakhov <damirsalakhov@gmail.com>
2025-10-28 15:35:17 +0500

    Remove database migration and disable migration checks

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Gamerthor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    
+    # Disable pending migration checks
+    config.active_record.check_pending_migrations = false
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
